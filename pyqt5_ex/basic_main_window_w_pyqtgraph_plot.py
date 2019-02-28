31f7d135129c99da52ad1d58ba23ef03eca4547c
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2019-02-28 11:59:44 -0500

    Clean to the bare minimum

--- pyqt5_ex/basic_main_window_w_pyqtgraph_plot.py
+++ pyqt5_ex/basic_main_window_w_pyqtgraph_plot.py
@@ -6,7 +6,6 @@ import sys
 
 
 class MainWin(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -20,35 +19,11 @@ class MainWin(QMainWindow):
 class PlotWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.layout = QGridLayout(self)
-
-        # p = self.create_plot()
-        b1 = QPushButton('yoo')
-        b2 = QPushButton('allo')
-
+        layout = QGridLayout(self)
         q = deque(np.random.random(100), maxlen=100)
-        q2 = deque(np.ones(100), maxlen=100)
-        # p = pg.plot()
-        p = pg.PlotWidget()
-        self.layout.addWidget(p)
-        # p2 = pg.plot(q2)
-        c = p.plot(q2)
-
-        self.layout.addWidget(b2)
-        self.layout.addWidget(b1)
-        # p_w = pg.PlotWidget()
-        # self.curve = p_w.plot(q2)
-        # c = self.curve.setData(q2)
-
-        # self.setLayout(self.layout)
-    # def create_plot(self):
-    #     q = deque(np.random.random(100), maxlen=100)
-    #     q2 = deque(np.ones(100), maxlen=100)
-    #     p = pg.plot()
-    #     p2 = pg.plot(q2)
-        # c = p.plot(q2)
-        # return p
-
+        plot = pg.PlotWidget()
+        layout.addWidget(plot)
+        plot.plot(q)
 
 
 if __name__ == '__main__':
