5e8a522ad6fb9c582cb075a0594237e15741cb83
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2019-02-28 11:39:57 -0500

    To correct the problem we need to use a pg.PlotWidget
    We need to use  pg.plotwidget rather than pg.plot which create a separate window for the plot

--- pyqt5_ex/basic_main_window_w_pyqtgraph_plot.py
+++ pyqt5_ex/basic_main_window_w_pyqtgraph_plot.py
@@ -20,22 +20,35 @@ class MainWin(QMainWindow):
 class PlotWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.layout = QHBoxLayout(self)
+        self.layout = QGridLayout(self)
+
+        # p = self.create_plot()
+        b1 = QPushButton('yoo')
+        b2 = QPushButton('allo')
 
         q = deque(np.random.random(100), maxlen=100)
         q2 = deque(np.ones(100), maxlen=100)
-
-
-        p = pg.plot(q)
+        # p = pg.plot()
+        p = pg.PlotWidget()
+        self.layout.addWidget(p)
+        # p2 = pg.plot(q2)
         c = p.plot(q2)
-        b = QPushButton('allo')
-        self.layout.addWidget(b)
+
+        self.layout.addWidget(b2)
+        self.layout.addWidget(b1)
         # p_w = pg.PlotWidget()
         # self.curve = p_w.plot(q2)
         # c = self.curve.setData(q2)
 
-        self.layout.addWidget(p)
-        self.setLayout(self.layout)
+        # self.setLayout(self.layout)
+    # def create_plot(self):
+    #     q = deque(np.random.random(100), maxlen=100)
+    #     q2 = deque(np.ones(100), maxlen=100)
+    #     p = pg.plot()
+    #     p2 = pg.plot(q2)
+        # c = p.plot(q2)
+        # return p
+
 
 
 if __name__ == '__main__':
